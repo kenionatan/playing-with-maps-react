87967686fc64b0aef3921b0807d433db0c069499
Kenio Natan <kenionatan@gmail.com>
2019-07-10 16:13:39 -0300

    Styling map

--- src/components/SimpleMap.js
+++ src/components/SimpleMap.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import mapStyle from '../mapstyle/mapstyle';
+import "./SimpleMap.css";
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const AnyReactComponent = ({ text }) =>
+    <div className="marker">
+        {text}
+    </div>;
 
 class SimpleMap extends Component {
     static defaultProps = {
@@ -20,7 +24,15 @@ class SimpleMap extends Component {
 
         return (
             // Important! Always set the container height explicitly
-            <div style={{ height: '400px', width: '800px' }}>
+            [
+            <div className="categories">
+                <div className="category">Restaurantes</div>
+                <div className="category">Bares</div>
+                <div className="category">Pubs</div>
+                <div className="category">Hotéis</div>
+            </div>,
+
+            <div style={{ height: '400px', width: '800px', marginTop: '38px' }}>
                 <GoogleMapReact
                     bootstrapURLKeys={{ key: 'AIzaSyAkGWCgbnd31w6kYrRZKQOy4VNqZa9JocM' }}
                     defaultCenter={this.props.center}
@@ -30,10 +42,17 @@ class SimpleMap extends Component {
                     <AnyReactComponent
                         lat={-9.654459}
                         lng={-35.706921}
-                        text="My Marker"
+                        text="Hotel Ritz Suítes.."
+                    />
+
+                    <AnyReactComponent
+                        lat={-9.642120}
+                        lng={-35.703230}
+                        text="Rex Jazz.."
                     />
                 </GoogleMapReact>
             </div>
+            ]
         );
     }
 }
